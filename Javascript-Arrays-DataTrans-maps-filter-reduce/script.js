5696865a99998385eedfadb8149cafa98385fa83
chithra98devi <44505676+chithra98devi@users.noreply.github.com>
2022-02-07 06:23:33 -0800

    Update script.js

--- Javascript-Arrays-DataTrans-maps-filter-reduce/script.js
+++ Javascript-Arrays-DataTrans-maps-filter-reduce/script.js
@@ -23,3 +23,61 @@ const movementsDescriptions = movements.map(
     )}`
 );
 console.log(movementsDescriptions);
+
+//=======================================
+//Filter Method
+//=======================================
+
+const deposits = movements.filter(function(mov){
+  return mov>0;
+});
+
+console.log(movements);
+console.log(deposits);
+
+const depositsFor =[];
+
+for (const mov of movements) if(mov >0) depositsFor.push(mov);
+console.log(depositsFor);
+
+const withdrawals = movements.filter(function(mov){
+  return mov < 0;
+});
+
+console.log(withdrawals);
+
+const withdrawalNew = movements.filter(mov => mov < 0);
+console.log(withdrawalNew);
+
+//=======================================
+//Reduce Method
+//=======================================
+
+//accumulator -> snowball
+const balance = movements.reduce(function(acc,current, i, arr){
+  console.log(`Iteration ${i}:${acc}`);
+  return acc + current; 
+},0)
+
+console.log(balance);
+
+//Arrow method
+
+const balance3 = movements.reduce((acc,current)=> acc+current,0)
+
+console.log(balance3);
+
+
+let balance2 =0;
+
+for(const mov of movements)balance2 += mov;
+console.log(balance2);
+
+//Max value
+
+const max = movements.reduce((acc,mov)=>{
+  if(acc > mov) return acc;
+  else return mov;
+},movements[0]);
+
+console.log(max);
