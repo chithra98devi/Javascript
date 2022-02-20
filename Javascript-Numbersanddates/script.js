4b0dd39dd2b9e9516e33bea7832247c1ffb11540
chithra98devi <44505676+chithra98devi@users.noreply.github.com>
2022-02-20 05:48:06 -0800

    Update script.js

--- Javascript-Numbersanddates/script.js
+++ Javascript-Numbersanddates/script.js
@@ -107,3 +107,106 @@ const isEven = n => n%2 ===0;
 console.log(isEven(4));
 console.log(isEven(23));
 console.log(isEven(512));
+
+//287,460,000,000
+const diameter = 287_460_000_000;
+console.log(diameter);
+
+
+const priceCents = 345_99;
+console.log(priceCents);
+
+
+const transferedFee1 = 15_00;
+const transferedFee2 = 1_500;
+
+
+const PI = 3.14_15;
+console.log(PI);
+
+console.log(Number('230000'));
+
+
+//Big int
+
+console.log(54657687687989809090909986n);
+
+console.log(BigInt(54657687687989809090909986));
+
+//operation
+
+console.log(10000n + 10000n);
+
+
+//Exceptions
+
+console.log(20n > 15);
+console.log(20n === 15);
+console.log(20n == '20');
+
+//Creating dates and times
+//create date
+
+const now = new Date();
+
+console.log(now);
+
+console.log(new Date('December 24, 2015'));
+console.log(new Date(2037,10,19,15,23,5));
+console.log(new Date(2037,10,31));
+
+
+console.log(new Date(0));
+console.log(new Date(3*24*60*60*1000));
+
+
+//working with dates
+
+const future = new Date(2037,10,19,15,23,5);
+console.log(future);
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getDay());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.toISOString());
+console.log(future.getTime());
+
+console.log(new Date(2142285785000));
+
+console.log(Date.now());
+
+future.setFullYear(2040);
+console.log(future);
+
+
+//calculating dates
+
+const future1 = new Date(2037,10,19,15,23,5);
+
+console.log(Number(future1));
+
+const calcDaysPassed = (date1 , date2) => Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
+
+const days1 =calcDaysPassed(new Date(2037,3,14), new Date(2037,3,24));
+
+console.log(days1);
+
+
+// Internationalization with dates
+
+const num = 3884764.23;
+
+const options ={
+    style: 'currency',
+    unit : 'celsius',
+    currency:'EUR',
+    // useGrouping:false
+}
+
+console.log('US',new Intl.NumberFormat('en-US',options).format(num));
+console.log('germany',new Intl.NumberFormat('de-DE').format(num));
+
+console.log(navigator.language, new Intl.NumberFormat(navigator.language).format(num));
